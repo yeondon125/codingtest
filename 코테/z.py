dd0ffab70198f99002da7f27bb63aa2576e8c888
yeondon <s25030@gsm.hs.kr>
2025-09-09 22:34:55 +0900

    z 성공

---
+++ 코테/z.py
@@ -0,0 +1,11 @@
+def solution(s):
+    s=s.split(" ")
+    answer = 0
+    for i in range(len(s)):
+        if s[i] == "Z":
+            answer -= int(s[i-1])
+        else:
+            answer += int(s[i])
+    return answer
+
+print(solution("1 2 Z 3"))  # 4
