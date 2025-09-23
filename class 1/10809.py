7a2fc6e9cacdec8d3c2bdb41651c090b8c58450d
yeondon <s25030@gsm.hs.kr>
2025-09-23 11:54:10 +0900

    10809

--- class 1/10809.py
+++ class 1/10809.py
@@ -1,6 +1,14 @@
 import sys
 input=sys.stdin.readline
-a=input()
-b=a.split()
+m=[-1]*26
+n=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-print(len(b))
+a=input()
+
+for i in a:
+  for idx, j in enumerate(n):
+    if i==j and m[idx]==-1:
+      m[idx]=a.index(i)
+
+for k in range(len(m)):
+  print(m[k], end=' ')
