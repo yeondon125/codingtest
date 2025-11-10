b0ef3dfc10ddd818ed11a226bdb6b73f6597d4dd
yeondon <s25030@gsm.hs.kr>
2025-11-10 22:58:40 +0900

    이진탐색트리

---
+++ 트리/2tt.py
@@ -0,0 +1,14 @@
+def search(tree, value):
+    index=1
+    node = tree[index]
+    while node is not None:
+      if value == node:
+        return True
+      elif value < node:
+        index = index * 2
+      else:
+        index = index * 2 + 1
+      if index >= len(tree):
+        break
+      node = tree[index] 
+    return False
